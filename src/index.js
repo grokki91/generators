790b9303922aee19afa9782a5812f9a51a8edf40
Pavel Chekalin <grokki91@gmail.com>
2023-01-29 20:04:20 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const team = new Team();
 const demon = new Character('Демон', 'Demon');
 const mag = new Character('Маг', 'Mag');
 team.addAll(demon, mag);
+
 for (const key of team) {
   console.log(key);
 }
